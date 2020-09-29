d2bf3998e0ca1ec00c0a11dd0e1642bf54e6c2d3
xstater <a166224@163.com>
2020-09-29 15:48:39 +0800

    add some test code

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
+pub trait Num{
+    fn number(self) -> Self{
+        self
+    }
+}
 
+impl Num for u8;
+impl Num for u16;
+impl Num for u32;
---
+++ tests/basic_tests.rs
@@ -0,0 +1,6 @@
+extern crate xrunits;
+
+#[test]
+fn num_trait_test(){
+    assert_eq!(123_u8.number(),123_u8);
+}
